4702d20201e27d6e2a1fef2dc3240d4e3968257b
H-junpei <hayakawa.junpei@consulting.headwaters.co.jp>
2026-06-22 18:15:34 +0900

    branch移動のため2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,6 @@ import AdminSelect from "./pages/AdminSelect";
 import AdminRegister from "./pages/AdminRegister"
 import AdminLogin from "./pages/AdminLogin";
 import AdminPage from "./pages/AdminPage";
-import UserPage from "./pages/UserPage";
 import Reservation from "./pages/reservation";
 import AdminJobSeekerLinkPage from "./pages/AdminJobSeekerLinkPage";
 import JobSeekerRegister from "./pages/JobSeekerRegister";
