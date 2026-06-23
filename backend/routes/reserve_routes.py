a5216b3494988a30afcba98f06a3ab3325b42971
H-junpei <hayakawa.junpei@consulting.headwaters.co.jp>
2026-06-23 15:13:21 +0900

    マージのため

--- backend/routes/reserve_routes.py
+++ backend/routes/reserve_routes.py
@@ -2,8 +2,7 @@ from flask import Blueprint, jsonify, request
 from datetime import datetime
 
 from extensions import db
-from models import TimeSlot
-
+from models import TimeSlot, Reservation, JobSeeker
 reserve_bp = Blueprint("reserve_bp", __name__)
 
 @reserve_bp.route("/api/reserve", methods=["POST"])
@@ -15,7 +14,24 @@ def reserve():
     name = data.get("name")
 
     if not day or not time:
-        return jsonify({"message": "day と time は必須です"}), 400
+        
+      if not name:
+          return jsonify({"message": "name は必須です"}), 400
+
+      job_seeker = JobSeeker.query.filter_by(name=name).first()
+
+      if not job_seeker:
+          return jsonify({"message": "求職者が見つかりません"}), 404
+
+      existing_reservation = Reservation.query.filter_by(
+          job_seeker_id=job_seeker.job_seeker_id
+      ).first()
+
+    if existing_reservation:
+        return jsonify({
+            "message": "すでに別の枠を予約済みです。1人1件までです"
+          }), 400
+
 
     try:
         dt = datetime.strptime(f"{day} {time}", "%Y-%m-%d %H:%M")
@@ -30,7 +46,13 @@ def reserve():
     if slot.status != "available":
         return jsonify({"message": "既に予約済み"}), 400
 
-    slot.status = "reserved"
+    new_reservation = Reservation(
+        time_slot_id=slot.time_slot_id,
+        job_seeker_id=job_seeker.job_seeker_id,
+        status="active"
+    )
+    db.session.add(new_reservation)
+    slot.status = "pending"
     db.session.commit()
 
     print("✅ 予約:", {
