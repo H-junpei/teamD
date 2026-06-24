0aa4bf129d9282b9a074a5e0ffe17854447db69d
3tuki <yotsuzuka.mitsuki@consulting.headwaters.co.jp>
2026-06-24 09:54:54 +0900

    ログインUI修正

--- frontend/src/pages/AdminSelect.jsx
+++ frontend/src/pages/AdminSelect.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-
+import "../Login.css";
 function AdminSelect() {
   const navigate = useNavigate();
 
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,35 +1,40 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import "../Login.css";
 
 const Login = () => {
-  // 求職者用
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
 
   const navigate = useNavigate();
 
-  // 管理者ボタンを押したら、管理者メニューへ移動
   const handleAdminClick = () => {
     navigate("/admin");
   };
 
-  // 求職者ボタンを押したら、求職者メニューへ移動
   const handleJobSeekerClick = () => {
-    navigate("/jobseeker")
-  }
+    navigate("/jobseeker");
+  };
 
   return (
-    <div>
-      <h2>ログイン</h2>
+    <div className="login-container">
+      <div className="login-card">
+        <h2 className="login-title">Log in</h2>
 
-      <button onClick={handleAdminClick}>管理者</button>
+        <button
+          className="login-button admin-button"
+          onClick={handleAdminClick}
+        >
+          👔 管理者
+        </button>
 
-      <br />
-      <br />
-
-      <h3>求職者ログイン</h3>
-
-      <button onClick={handleJobSeekerClick}>求職者ログイン</button>
+        <button
+          className="login-button jobseeker-button"
+          onClick={handleJobSeekerClick}
+        >
+          🙋‍♂️ 求職者
+        </button>
+      </div>
     </div>
   );
 };
